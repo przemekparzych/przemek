9a42120c87fe95ab40ef3dbc54b077f4a281b364
przemekparzych <pp.18@op.pl>
2016-01-29 16:34:05 +0100

    add new class

---
+++ Asp.NetMvcNoAuthentication/Test folder/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp.NetMvcNoAuthentication.Test_folder
+{
+    public class Class1
+    {
+    }
+}
